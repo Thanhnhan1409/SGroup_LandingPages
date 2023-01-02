756b7e44637c5b98eee8200f24a5ad516745383f
ThanhNhan <phanthithanhnhan1409@gmail.com>
2023-01-02 21:46:00 +0700

    LP

--- main.js
+++ main.js
@@ -57,3 +57,58 @@ document.addEventListener("DOMContentLoaded",function() {
 		}
 	})
 	})
+
+// slick slider
+$(document).ready(function () {
+    $(".slick-slider").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      autoplay: true,
+      autoplaySpeed: 2000,
+	  cssEase: 'linear',
+    });
+  });
+//scroll header
+  document.addEventListener("DOMContentLoaded",function() {
+	var menu = document.querySelectorAll('header.navbar');
+	var menu = menu[0];
+		//Truy xuất header
+		var trangthai="duoi400";
+	window.addEventListener("scroll",function(){
+	var x = pageYOffset;
+	if(x > 400){
+		if(trangthai == "duoi400")
+		{
+			trangthai="tren400";
+			menu.classList.add('navbar-sticky');
+		}
+	}
+	else{
+		if(trangthai=="tren400"){
+			menu.classList.remove('navbar-sticky');
+			trangthai="duoi400";}
+		}
+	})
+	})
